5e40f78547c69a8b28d4c4498b3c5b97738b4f9d
moqimoqidea <moqimoqidea@gmail.com>
2024-08-05 02:13:01 +0800

    update +1

--- tests/test_core_exception.py
+++ tests/test_core_exception.py
@@ -53,6 +53,32 @@ class DemoService:
         # noinspection PyArgumentList
         return exception_class(message=message)
 
+    def dict_and_path_to_exception(self, data: dict, exception_path: str) -> DemoException:
+        module_path, _, class_name = exception_path.rpartition('.')
+        if not module_path:
+            raise ValueError("You must provide the full path to the exception class, including the module.")
+
+        try:
+            module = importlib.import_module(module_path)
+        except ImportError as e:
+            raise ImportError(f"Module {module_path} could not be loaded: {str(e)}")
+
+        exception_class = getattr(module, class_name, None)
+        if exception_class is None:
+            raise ValueError(f"No exception class found with the name: {class_name} in module {module_path}")
+
+        if not issubclass(exception_class, Exception):
+            raise TypeError(f"{class_name} is not a subclass of Exception")
+
+        constructor_params = exception_class.__init__.__code__.co_varnames[
+                             1:exception_class.__init__.__code__.co_argcount]
+        missing_params = [param for param in constructor_params if param not in data]
+        if missing_params:
+            raise ValueError(f"Missing required parameters {missing_params} for initializing '{class_name}'.")
+
+        # noinspection PyArgumentList
+        return exception_class(**data)
+
 
 demo_service = DemoService()
 
@@ -72,3 +98,9 @@ def test_str_and_path_to_exception():
     message = "An error occurred"
     exception = demo_service.str_and_path_to_exception(message, "tests.test_core_exception.DemoException")
     assert exception.message == message
+
+
+def test_dict_and_path_to_exception():
+    exception_dict = {"message": "An error occurred"}
+    exception = demo_service.dict_and_path_to_exception(exception_dict, "tests.test_core_exception.DemoException")
+    assert exception.message == exception_dict["message"]
